84b1778fa0fba34b95c0a729daeec62e0f4a5e3f
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-08-31 17:14:52 -0500

    Added all animals, kinda feel like I didn't do everything

---
+++ Animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    abstract class Animal
+    {
+        public string Eat { get; set; }
+
+        public virtual void Sleep()
+        {
+            Console.WriteLine("I sleep at night like the average human.");
+        }
+
+        public abstract string Sound { get; }
+
+        public abstract string Home { get; }
+    }
+}
---
+++ Animals/Fish.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    abstract class Fish : Animal
+    {
+        public bool HasGills { get; set; }
+
+        public bool LivesInOcean { get; set; }
+
+        public abstract bool Omnivore { get; }
+
+        public bool Carnivore => !Omnivore;
+
+        public override string Sound { get; }
+
+        public override string Home { get; }
+
+        public override void Sleep()
+        {
+            base.Sleep();
+        }
+    }
+
+    class ClownFish : Fish
+    {
+        public override void Sleep()
+        {
+            base.Sleep();
+            Console.WriteLine("I sleep in an anenome, and it is actively awake while I'm sleeping.");
+        }
+
+        public override bool Omnivore => true;
+
+        public override string Sound => "Chirp";
+        public override string Home => "Anemone";
+    }
+
+    class GreatWhiteShark : Fish
+    {
+        public override void Sleep()
+        {
+            Console.WriteLine("I don't really sleep like people do.");
+            Console.WriteLine("Because I have to keep swimming at all times (or die)");
+            Console.WriteLine("I just take naps often periodically through the day while I can drift forward.");
+        }
+
+        public override bool Omnivore => false;
+
+        public override string Sound => "Silence";
+        public override string Home => "Ocean";
+    }
+}
